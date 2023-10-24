f215c087c94adb37ae522a71deb10aa5784cad60
Francisco Diaz <franciscodiazhuerta97@gmail.com>
2023-10-24 14:29:42 -0600

    se agregaran algunos estilos y validacion al formularios

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ window.onload = () => {
   document.querySelector(".arrow-left").addEventListener("click", clickLeft);
   document
     .querySelector(".send-button")
-    .addEventListener("click", showNotification);
+    .addEventListener("click", e =>validateForm(e));
   document.querySelectorAll(".project").forEach(element => {
     element.addEventListener("click", e => openModal(e));
   });
@@ -69,9 +69,25 @@ function clickLeft() {
   }
 }
 
+function validateForm(e){
+  e.preventDefault()
+  const nameField = document.getElementById("name")
+  console.log(nameField)
+  if(nameField.value == ""){
+    document.getElementById("name-error").innerHTML = "para enviar el formulario se necetia un nombre";
+  }else{
+    showNotification()
+  }
+}
+
+
 /** Esta funcion se llama cuando la persona hace click en el boton de enviar del formulario de contacto */
 function showNotification() {
+  document.getElementById("name-error").innerHTML ="";
+  document.querySelector(".form-container").reset();s
+ 
   document.querySelector(".notification").style.display = "flex";
+  document.querySelector(".notification").innerHTML = "El formulario fue enviado sin errores"
   setTimeout(function() {
     document.querySelector(".notification").style.display = "none";
   }, 3000);
